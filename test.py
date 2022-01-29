17458ac58917bc0ed26c756e1ac5377d39e46358
Fernando Sanchez <32527936+fsanchez334@users.noreply.github.com>
2022-01-29 00:32:03 -0500

    Contains examples to test the trie implementation

---
+++ test.py
@@ -0,0 +1,32 @@
+from trie import Trie, build_big_trie, isPresent, make_small_tries
+
+#Example 1: In this example, we are building a trie structure for just one word
+word = 'car'
+container = list(word)
+tries = []
+for i in range(len(container)-1):
+  holder = Trie(container[i])
+  holder.insertChild(Trie(container[i + 1]))
+  tries.append(holder)
+
+#Example 2: In this example, we are building a node for each character that is present in the dictionary
+dictionary = ["car", "card", "cards", "cot", "cots", "trie", "tried", "tries", "try"]
+root = make_small_tries(dictionary)
+
+#Example 3: Finally, we are building the trie data structure based on the words in the dictionary
+beginner = build_big_trie(root, dictionary)
+
+#Here we test that our trie is able to recognize the words in our dictionary
+print("-----------Testinng the Trie---------------------")
+for word in dictionary:
+    print("Is", word, "present?", isPresent(root, word))
+print("-------------------------------------------------")
+
+#In the following lines, we test what happens when new words are tested against our trie
+print("-----------Testinng the Trie against unseen words---------------------")
+
+bad_words = ["cars", "candy", "cott", "triez"]
+for words in bad_words:
+    print("Is", words, "present?", isPresent(root, words))
+
+print("----------------------------------------------------------------------")
